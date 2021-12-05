c025a3fb9ab70731593fc6ae50dd669bdc05f993
Copperfield19911014 <cpf19911014@126.com>
2021-12-05 23:14:34 +0800

    新增

---
+++ long-list/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <list />
+  </div>
+</template>
+
+<script>
+import list from './components/list.vue'
+
+export default {
+  name: 'App',
+  components: {
+    list
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ long-list/src/components/list.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="div_body">
+        <div class="div_container">
+            <div ref="phantom" class="phantom" ></div>
+            <table class="table">
+                <!-- <tr>
+                    <th>序号</th>
+                    <th>姓名</th>
+                    <th>年龄</th>
+                    <th>性别</th>
+                    <th>生日</th>
+                    <th>身份</th>
+                </tr> -->
+                <tr class="tr" :key="index" v-for="(item, index) in list">
+                    <td>序号:{{item.index}}</td>
+                    <td>姓名:{{item.index}}</td>
+                    <td>年龄:{{item.index}}</td>
+                    <td>性别:{{item.index}}</td>
+                    <td>生日:{{item.index}}</td>
+                    <td>身份:{{item.index}}</td>
+                    <td>序号:{{item.index}}</td>
+                    <td>姓名:{{item.index}}</td>
+                    <td>年龄:{{item.index}}</td>
+                    <td>性别:{{item.index}}</td>
+                    <td>生日:{{item.index}}</td>
+                    <td>身份:{{item.index}}</td>
+                    
+                </tr>
+            </table>
+        </div>
+        
+    </div>
+    
+</template>
+<script>
+
+export default{
+    data() {
+        return {
+            list :[],
+            showList: [],
+            startIndex: 0,
+            endIndex: 10
+        }
+    },
+    created(){
+        let _this = this
+        let num = 1000
+        for(let i = 0;i < num;i++){
+            _this.list.push({
+                index: i
+            })
+        }
+        this.showList = this.list.splice(this.startIndex, this.endIndex)
+        
+    },
+    mounted(){
+        this.setPhantomHeight()
+    },
+    methods: {
+        setPhantomHeight(){
+            this.$refs.phantom.style.height = 60 * this.list.length + 'px'
+            console.log(this.$refs.phantom.clientHeight)
+
+        }
+    }
+}
+</script>
+<style >
+    .div_body{
+        width: 90%;
+        margin: auto;
+        height:600px;
+        border: solid 3px #ccc;
+    }
+    .div_container{
+        position: relative;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+    }
+    .phantom{
+        border: solid 2px red;
+    }
+    .table{
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
+    .tr{
+        height: 60px !important;
+    }
+</style>
